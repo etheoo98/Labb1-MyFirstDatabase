70599436d454df4bbeb1330e40e1f3e01006144b
etheoo <etheoo@protonmail.com>
2023-11-29 19:00:35 +0100

    Revert "Initial Commit"
    This reverts commit 6da15d9a1fadafd0e7f4731ca5dd576434c403e8.

--- Roles.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO TestDatabase.dbo.Roles (Role) VALUES (N'Rifler');
-INSERT INTO TestDatabase.dbo.Roles (Role) VALUES (N'AWPer');
-INSERT INTO TestDatabase.dbo.Roles (Role) VALUES (N'In-game leader');
