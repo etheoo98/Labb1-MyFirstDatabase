11e181ee0b3dac5a2bdc962ee0895804dcd876e7
etheoo <etheoo@protonmail.com>
2023-11-29 23:17:55 +0100

    Update sql source path

--- setup.bat
+++ setup.bat
@@ -5,8 +5,8 @@ set SERVER=(localdb)\MSSQLLocalDB
 set DATABASE=MyFirstDatabase
 
 %SQLCMD% -S %SERVER% -E -Q "IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = '%DATABASE%') CREATE DATABASE %DATABASE%"
-%SQLCMD% -S %SERVER% -d %DATABASE% -E -i ".\schema.sql"
-%SQLCMD% -S %SERVER% -d %DATABASE% -E -i ".\data.sql"
+%SQLCMD% -S %SERVER% -d %DATABASE% -E -i ".\src\schema.sql"
+%SQLCMD% -S %SERVER% -d %DATABASE% -E -i ".\src\data.sql"
 
 echo If no error was encountered, you're good to go! Otherwise read the documentation for troubleshooting.
 
